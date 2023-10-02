1cd9c1982d07a14fb27d7fcef04fb61f5df50d61
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2023-10-01 21:04:09 -0400

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,11 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> year=eval(input("Enter a year"))
+Enter a year 2016
+>>> if year % 4==0:
+...     print("Leap year")
+... else:
+...     print("not a leap year")
+... 
+...     
+Leap year
